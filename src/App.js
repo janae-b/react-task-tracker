5aa4f876c3c2260d483558bd14bda39b36163450
janae-b <janae.boersma@comcast.net>
2021-06-01 14:52:02 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,18 +25,29 @@ function App() {
       },
     ])
 
+    // Add Task
+    const addTask = (task) => {
+      console.log
+    }
+
     // Delete Task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !==id))
     }
     //Toggle Reminder
     const toggleReminder = (id) => {
-      console.log(id)
+      setTasks(
+        tasks.map((task) => 
+          task.id === id ? { ...task, reminder: 
+          !task.reminder } : task
+        )
+      )
     }
 
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} 
         onDelete={deleteTask} onToggle={toggleReminder} />
